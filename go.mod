43e179cc36771fa7ffb6848b628c919363e02927
Christian Simon <simon@swine.de>
2023-05-07 15:27:08 +0100

    Add support for eddi
    This also adds load power monitoring and a last seen metric

--- go.mod
+++ go.mod
@@ -5,20 +5,21 @@ go 1.19
 require (
 	github.com/icholy/digest v0.1.22
 	github.com/k0kubun/pp/v3 v3.2.0
-	github.com/prometheus/client_golang v1.14.0
+	github.com/prometheus/client_golang v1.15.1
+	golang.org/x/sync v0.1.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.37.0 // indirect
-	github.com/prometheus/procfs v0.8.0 // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.43.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
--- zappi.go
+++ zappi.go
@@ -3,19 +3,21 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"flag"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/icholy/digest"
 	"github.com/k0kubun/pp/v3"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/sync/errgroup"
 )
 
 type ConnectorStatus string
@@ -45,6 +47,107 @@ func (m ConnectorStatus) String() string {
 	return "unknown"
 }
 
+type EddiStatus int
+
+const (
+	EddiStatusUnknown EddiStatus = iota
+	EddiStatusPaused
+	_
+	EddiStatusDiverting
+	_
+	EddiStatusMaxTempReached
+	EddiStatusStopped
+)
+
+var eddiStatuses = map[EddiStatus]string{
+	EddiStatusUnknown:        "unknown",
+	EddiStatusPaused:         "paused",
+	EddiStatusDiverting:      "diverting",
+	EddiStatusMaxTempReached: "max-temp-reached",
+	EddiStatusStopped:        "stopped",
+}
+
+func (s EddiStatus) String() string {
+	if v, ok := eddiStatuses[s]; ok {
+		return v
+	}
+	return "unknown"
+}
+
+// see https://github.com/twonk/MyEnergi-App-Api/blob/23a718a1ec8b3dd09842f312a0d79b249733f19f/README.md?plain=1#L130
+type Eddi struct {
+	SerialNumber       uint64     `json:"sno"`   // Eddi Serial Number
+	Date               string     `json:"dat"`   // date
+	Time               string     `json:"tim"`   // time
+	Ectp1              int        `json:"ectp1"` // physical CT connection 1 value
+	Ectp2              int        `json:"ectp2"` // physical CT connection 2 value
+	Ectp3              int        `json:"ectp3"` // physical CT connection 3 value
+	Ectt1              string     `json:"ectt1"` // CT 1 name
+	Ectt2              string     `json:"ectt2"` // CT 2 name
+	Ectt3              string     `json:"ectt3"` // CT 3 name
+	BoostMode          uint64     `json:"bsm"`   // Boost Mode - 1 if boosting
+	RemainingBoostTime int        `json:"rbt"`   // If boosting, the remaining boost time in of seconds
+	SupplyFrequency    float64    `json:"frq"`   // Supply Frequency
+	SupplyVoltage      int        `json:"vol"`   // Voltage out (divided by 10)
+	Gen                int        `json:"gen"`   //Generated Watts
+	FirmwareVersion    string     `json:"fwv"`
+	GridPower          int        `json:"grd"` // Current Watts from Grid (negative if sending to grid)
+	Hno                int        `json:"hno"` // Currently active heater (1 or 2)
+	Pha                int        `json:"pha"` // Phase number of phases?
+	Status             EddiStatus `json:"sta"` // Status of Eddi 1=Paused 3=Diverting 5=Max Temp Reached 6=Stopped
+	Ht1                string     `json:"ht1"` // Heater 1 name
+	Ht2                string     `json:"ht2"` // Heater 2 name
+	Tp1                int        `json:"tp1"` // Temperature probe 1 (in Celsius)
+	Tp2                int        `json:"tp2"` // Temperature probe 2 (in Celsius)
+	Pri                int        `json:"pri"` // Priority
+	Cmt                int        `json:"cmt"` // Command Timer - counts 1 - 10 when command sent, then 254 - success, 253 - failure, 255 - never received any commands
+	R1A                int        `json:"r1a"`
+	R2A                int        `json:"r2a"`
+	R1B                int        `json:"r1b"`
+	R2B                int        `json:"r2b"`
+	Che                float64    `json:"che"` // total kWh tranferred this session (today?)
+	Dst                int        `json:"dst"` // Daylight Savings Time enabled
+
+}
+
+func collectDateTime(dateS, timeS string, m prometheus.Gauge) {
+	t, err := time.Parse("02-01-2006 15:04:05", dateS+" "+timeS)
+	if err != nil {
+		log.Printf("failed to parse time: %v", err)
+		return
+	}
+	m.Set(float64(t.UnixNano()) / 1e9)
+}
+
+func (e *Eddi) collect(m *metrics) {
+	serial := strconv.FormatUint(e.SerialNumber, 10)
+	model := "eddi"
+
+	m.info.WithLabelValues(model, serial, e.FirmwareVersion).Set(1)
+
+	for s := range eddiStatuses {
+		value := 0.0
+		if s == e.Status {
+			value = 1.0
+		}
+		m.status.WithLabelValues(model, serial, s.String()).Set(value)
+	}
+
+	m.gridPower.WithLabelValues(model, serial).Set(float64(e.GridPower))
+	m.supplyVoltage.WithLabelValues(model, serial).Set(float64(e.SupplyVoltage) / 10)
+	m.supplyFreq.WithLabelValues(model, serial).Set(float64(e.SupplyFrequency))
+	if e.Ectt1 == "Internal Load" {
+		m.loadPower.WithLabelValues(model, serial).Set(float64(e.Ectp1))
+	}
+	if e.Ht1 != "None" {
+		m.temperature.WithLabelValues(model, serial, e.Ht1).Set(float64(e.Tp1))
+	}
+	if e.Ht2 != "None" {
+		m.temperature.WithLabelValues(model, serial, e.Ht2).Set(float64(e.Tp2))
+	}
+	collectDateTime(e.Date, e.Time, m.lastSeen.WithLabelValues(model, serial))
+}
+
 type ZappiMode int
 
 const (
@@ -69,26 +172,26 @@ func (m ZappiMode) String() string {
 	return "unknown"
 }
 
-type Status int
+type ZappiStatus int
 
 const (
-	StatusUnknown Status = iota
-	StatusPaused
+	ZappiStatusUnknown ZappiStatus = iota
+	ZappiStatusPaused
 	_
-	StatusCharging
+	ZappiStatusCharging
 	_
-	StatusComplete
+	ZappiStatusComplete
 )
 
-var statuses = map[Status]string{
-	StatusUnknown:  "unknown",
-	StatusPaused:   "paused",
-	StatusCharging: "charging",
-	StatusComplete: "complete",
+var zappiStatuses = map[ZappiStatus]string{
+	ZappiStatusUnknown:  "unknown",
+	ZappiStatusPaused:   "paused",
+	ZappiStatusCharging: "charging",
+	ZappiStatusComplete: "complete",
 }
 
-func (s Status) String() string {
-	if v, ok := statuses[s]; ok {
+func (s ZappiStatus) String() string {
+	if v, ok := zappiStatuses[s]; ok {
 		return v
 	}
 	return "unknown"
@@ -98,12 +201,16 @@ func (s Status) String() string {
 type Zappi struct {
 	SerialNumber            uint64          `json:"sno"`
 	Date                    string          `json:"dat"`
-	Timer                   string          `json:"tim"`
-	Ectp2                   int             `json:"ectp2"`
-	Ectt1                   string          `json:"ectt1"`
-	Ectt2                   string          `json:"ectt2"`
-	Ectt3                   string          `json:"ectt3"`
-	Bsm                     int             `json:"bsm"`
+	Time                    string          `json:"tim"`
+	Ectp1                   int             `json:"ectp1"` // physical CT connection 1 value
+	Ectp2                   int             `json:"ectp2"` // physical CT connection 2 value
+	Ectp3                   int             `json:"ectp3"` // physical CT connection 3 value
+	Ectp4                   int             `json:"ectp4"` // physical CT connection 4 value
+	Ectt1                   string          `json:"ectt1"` // CT 1 name
+	Ectt2                   string          `json:"ectt2"` // CT 2 name
+	Ectt3                   string          `json:"ectt3"` // CT 3 name
+	Ectt4                   string          `json:"ectt4"` // CT 4 name
+	BoostMode               uint64          `json:"bsm"`
 	Bst                     int             `json:"bst"`
 	Cmt                     int             `json:"cmt"`
 	Dst                     int             `json:"dst"`
@@ -112,7 +219,7 @@ type Zappi struct {
 	GridPower               int             `json:"grd"`
 	Pha                     int             `json:"pha"`
 	Pri                     int             `json:"pri"`
-	Status                  Status          `json:"sta"`
+	Status                  ZappiStatus     `json:"sta"`
 	Tz                      int             `json:"tz"`
 	SupplyFrequency         float64         `json:"frq"`
 	SupplyVoltage           int             `json:"vol"`
@@ -125,9 +232,6 @@ type Zappi struct {
 	Rdc                     int             `json:"rdc"`
 	Rrac                    int             `json:"rrac"`
 	Zsh                     int             `json:"zsh"`
-	Ectt4                   string          `json:"ectt4"`
-	Ectt5                   string          `json:"ectt5"`
-	Ectt6                   string          `json:"ectt6"`
 	NewAppAvailable         bool            `json:"newAppAvailable"`
 	NewBootloaderAvailable  bool            `json:"newBootloaderAvailable"`
 	BeingTamperedWith       bool            `json:"beingTamperedWith"`
@@ -137,75 +241,171 @@ type Zappi struct {
 	Sbk                     int             `json:"sbk"`
 }
 
-func getStatus(ctx context.Context) (*Zappi, error) {
-	req, err := http.NewRequest("GET", "https://s18.myenergi.net/cgi-jstatus-Z", nil)
+func (z *Zappi) collect(m *metrics) {
+	model := "zappi"
+	serial := strconv.FormatUint(z.SerialNumber, 10)
+
+	m.info.WithLabelValues(model, serial, z.FirmwareVersion).Set(1)
+
+	for s := range zappiStatuses {
+		value := 0.0
+		if s == z.Status {
+			value = 1.0
+		}
+		m.status.WithLabelValues(model, serial, s.String()).Set(value)
+	}
+
+	for s := range zappiModes {
+		value := 0.0
+		if s == z.ZappiMode {
+			value = 1.0
+		}
+		m.zappiMode.WithLabelValues(model, serial, s.String()).Set(value)
+	}
+
+	for s := range connectorStatuses {
+		value := 0.0
+		if s == z.ConnectorStatus {
+			value = 1.0
+		}
+		m.connectorStatus.WithLabelValues(model, serial, s.String()).Set(value)
+	}
+
+	m.gridPower.WithLabelValues(model, serial).Set(float64(z.GridPower))
+	if z.Ectt1 == "Internal Load" {
+		m.loadPower.WithLabelValues(model, serial).Set(float64(z.Ectp1))
+	}
+
+	m.supplyVoltage.WithLabelValues(model, serial).Set(float64(z.SupplyVoltage) / 10)
+	m.supplyFreq.WithLabelValues(model, serial).Set(float64(z.SupplyFrequency))
+	collectDateTime(z.Date, z.Time, m.lastSeen.WithLabelValues(model, serial))
+}
+
+func getEddiStatus(ctx context.Context, client *http.Client) ([]*Eddi, error) {
+	req, err := http.NewRequest("GET", "https://s18.myenergi.net/cgi-jstatus-E", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	client := &http.Client{
-		Transport: &digest.Transport{
-			Username: os.Getenv("ZAPPI_SERIAL"),
-			Password: os.Getenv("ZAPPI_API_KEY"),
-		},
-	}
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		_ = resp.Body.Close()
+	}()
 
 	var respStatus struct {
-		Zappi []Zappi `json:"zappi"`
+		Eddi []*Eddi `json:"eddi"`
 	}
 	if err := json.NewDecoder(resp.Body).Decode(&respStatus); err != nil {
 		return nil, err
 	}
 
-	if c := len(respStatus.Zappi); c == 0 {
-		return nil, errors.New("no zappi found")
-	} else if c > 1 {
-		return nil, errors.New("got more than a single zappi back")
+	return respStatus.Eddi, nil
+}
+
+func getZappiStatus(ctx context.Context, client *http.Client) ([]*Zappi, error) {
+	req, err := http.NewRequest("GET", "https://s18.myenergi.net/cgi-jstatus-Z", nil)
+	if err != nil {
+		return nil, err
 	}
 
-	return &respStatus.Zappi[0], nil
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_, _ = io.Copy(io.Discard, resp.Body)
+		_ = resp.Body.Close()
+	}()
 
+	var respStatus struct {
+		Zappi []*Zappi `json:"zappi"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&respStatus); err != nil {
+		return nil, err
+	}
+
+	return respStatus.Zappi, nil
+}
+
+func newClient() *http.Client {
+	return &http.Client{
+		Transport: &digest.Transport{
+			Username: os.Getenv("ZAPPI_SERIAL"),
+			Password: os.Getenv("ZAPPI_API_KEY"),
+		},
+	}
 }
 
 func run(ctx context.Context) error {
-	status, err := getStatus(ctx)
+	client := newClient()
+	zappis, err := getZappiStatus(ctx, client)
 	if err != nil {
 		return err
 	}
-	log.Printf("serial: %d", status.SerialNumber)
-	log.Printf("status: %s", status.Status)
-	log.Printf("zappi mode: %s", status.ZappiMode)
-	log.Printf("connector status: %s", status.ConnectorStatus)
 
-	for _, line := range strings.Split(pp.Sprint(status), "\n") {
-		log.Println(line)
+	for _, z := range zappis {
+		log.Printf("serial: %d", z.SerialNumber)
+		log.Printf("status: %s", z.Status)
+		log.Printf("zappi mode: %s", z.ZappiMode)
+		log.Printf("connector status: %s", z.ConnectorStatus)
+
+		for _, line := range strings.Split(pp.Sprint(z), "\n") {
+			log.Println(line)
+		}
 	}
+
+	eddis, err := getEddiStatus(ctx, client)
+	if err != nil {
+		return err
+	}
+
+	for _, e := range eddis {
+		log.Printf("serial: %d", e.SerialNumber)
+		log.Printf("status: %s", e.Status)
+
+		for _, line := range strings.Split(pp.Sprint(e), "\n") {
+			log.Println(line)
+		}
+	}
+
 	return nil
 }
 
 func newMetrics(reg prometheus.Registerer) *metrics {
-	namespace := "zappi"
+	namespace := "myenergi"
 	return &metrics{
-		info:            prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "info"}, []string{"serial", "firmware_version"}),
-		status:          prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "status"}, []string{"serial", "status"}),
-		zappiMode:       prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "mode"}, []string{"serial", "mode"}),
-		connectorStatus: prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "connector_status"}, []string{"serial", "status"}),
-		gridPower:       prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "grid_power_watt"}, []string{"serial"}),
-		supplyVoltage:   prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "supply_voltage"}, []string{"serial"}),
-		supplyFreq:      prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "supply_frequency_hz"}, []string{"serial"}),
+		client: newClient(),
+
+		info:            prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "info"}, []string{"model", "serial", "firmware_version"}),
+		status:          prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "status"}, []string{"model", "serial", "status"}),
+		zappiMode:       prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "zappi_mode"}, []string{"model", "serial", "mode"}),
+		connectorStatus: prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "connector_status"}, []string{"model", "serial", "status"}),
+		gridPower:       prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "grid_power_watt"}, []string{"model", "serial"}),
+		loadPower:       prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "load_power_watt"}, []string{"model", "serial"}),
+		supplyVoltage:   prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "supply_voltage"}, []string{"model", "serial"}),
+		supplyFreq:      prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "supply_frequency_hz"}, []string{"model", "serial"}),
+		temperature:     prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "temperature_celsius"}, []string{"model", "serial", "sensor"}),
+		lastSeen:        prometheus.NewGaugeVec(prometheus.GaugeOpts{Namespace: namespace, Name: "last_seen_unixtime"}, []string{"model", "serial"}),
 	}
 }
 
 type metrics struct {
+	client *http.Client
+
 	info            *prometheus.GaugeVec
 	status          *prometheus.GaugeVec
 	zappiMode       *prometheus.GaugeVec
 	connectorStatus *prometheus.GaugeVec
 	gridPower       *prometheus.GaugeVec
+	loadPower       *prometheus.GaugeVec
 	supplyVoltage   *prometheus.GaugeVec
 	supplyFreq      *prometheus.GaugeVec
+	temperature     *prometheus.GaugeVec
+	lastSeen        *prometheus.GaugeVec
 }
 
 func (m *metrics) Describe(ch chan<- *prometheus.Desc) {
@@ -214,56 +414,55 @@ func (m *metrics) Describe(ch chan<- *prometheus.Desc) {
 	m.zappiMode.Describe(ch)
 	m.connectorStatus.Describe(ch)
 	m.gridPower.Describe(ch)
+	m.loadPower.Describe(ch)
 	m.supplyVoltage.Describe(ch)
 	m.supplyFreq.Describe(ch)
+	m.temperature.Describe(ch)
+	m.lastSeen.Describe(ch)
+
 }
 
 func (m *metrics) Collect(ch chan<- prometheus.Metric) {
-	status, err := getStatus(context.Background())
-	if err != nil {
-		return
-	}
-
-	serial := strconv.FormatUint(status.SerialNumber, 10)
+	g, ctx := errgroup.WithContext(context.Background())
 
 	m.info.Reset()
-	m.info.WithLabelValues(serial, status.FirmwareVersion).Set(1)
 
-	for s := range statuses {
-		value := 0.0
-		if s == status.Status {
-			value = 1.0
+	g.Go(func() error {
+		zappis, err := getZappiStatus(ctx, m.client)
+		if err != nil {
+			return err
 		}
-		m.status.WithLabelValues(serial, s.String()).Set(value)
-	}
 
-	for s := range zappiModes {
-		value := 0.0
-		if s == status.ZappiMode {
-			value = 1.0
+		for _, z := range zappis {
+			z.collect(m)
 		}
-		m.zappiMode.WithLabelValues(serial, s.String()).Set(value)
-	}
 
-	for s := range connectorStatuses {
-		value := 0.0
-		if s == status.ConnectorStatus {
-			value = 1.0
+		return nil
+	})
+
+	g.Go(func() error {
+		eddis, err := getEddiStatus(ctx, m.client)
+		if err != nil {
+			return err
 		}
-		m.connectorStatus.WithLabelValues(serial, s.String()).Set(value)
-	}
 
-	m.gridPower.WithLabelValues(serial).Set(float64(status.GridPower))
-	m.supplyVoltage.WithLabelValues(serial).Set(float64(status.SupplyVoltage) / 10)
-	m.supplyFreq.WithLabelValues(serial).Set(float64(status.SupplyFrequency))
+		for _, e := range eddis {
+			e.collect(m)
+		}
+
+		return nil
+	})
 
 	m.info.Collect(ch)
 	m.status.Collect(ch)
 	m.zappiMode.Collect(ch)
 	m.connectorStatus.Collect(ch)
 	m.gridPower.Collect(ch)
+	m.loadPower.Collect(ch)
 	m.supplyVoltage.Collect(ch)
 	m.supplyFreq.Collect(ch)
+	m.temperature.Collect(ch)
+	m.lastSeen.Collect(ch)
 }
 
 func main() {
@@ -295,6 +494,6 @@ func main() {
 		},
 	))
 
-	log.Printf("zappi exporter listening on %s", *addr)
+	log.Printf("myenergi exporter listening on %s", *addr)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
